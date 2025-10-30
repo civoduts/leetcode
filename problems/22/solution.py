40e64f9f8e49d7dfad6381e9d35951c9987f3351
Mirko Studovic <mirkosec@gmail.com>
2025-10-30 14:48:57 +0100

    In problem 22, use stack to store current sequence

--- problems/22/solution.py
+++ problems/22/solution.py
@@ -1,27 +1,33 @@
 def dfs(
     ans: list[str],
-    seq: str,
+    buff: list[str],
     opens: int,
     closes: int,
     n: int,
 ) -> None:
     # goal
     if opens + closes == 2 * n:
-        ans.append(seq)
+        ans.append("".join(buff))
         return
-    
+
     # constraints
     if opens < n:
-        # choice 
-        dfs(ans, seq + "(", opens + 1, closes, n)
+        # choice
+        buff.append("(")
+        dfs(ans, buff, opens + 1, closes, n)
+        buff.pop()
     if closes < opens:
-        # choice 
-        dfs(ans, seq + ")", opens, closes + 1, n)
+        # choice
+        buff.append(")")
+        dfs(ans, buff, opens, closes + 1, n)
+        buff.pop()
+
 
 # backtracking
 class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
         ans = []
+        buff = []
 
-        dfs(ans, "", 0, 0, n)
+        dfs(ans, buff, 0, 0, n)
         return ans
