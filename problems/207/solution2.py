77b3b8bcf45e884ecf5e7f128d926e420f4f1153
Mirko Studovic <mirkosec@gmail.com>
2025-11-10 17:57:47 +0100

    Add consts for state tracking for 207

--- problems/207/solution2.py
+++ problems/207/solution2.py
@@ -4,25 +4,27 @@ from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
         graph = defaultdict(list)
-        state = [0] * numCourses
 
         for course, prereq in prerequisites:
             graph[prereq].append(course)
 
+        UNVISITED, VISITING, DONE = 0, 1, 2
+        state = [UNVISITED] * numCourses
+
         def dfs(course: int) -> bool:
-            if state[course] == 1:
+            if state[course] == VISITING:
                 return False
-            if state[course] == 2:
+            if state[course] == DONE:
                 return True
 
-            state[course] = 1
+            state[course] = VISITING
             for prereq in graph[course]:
                 if not dfs(prereq):
                     return False
-            state[course] = 2
+            state[course] = DONE
             return True
 
         for course in range(numCourses):
-            if state[course] == 0 and not dfs(course):
+            if state[course] == UNVISITED and not dfs(course):
                 return False
         return True
