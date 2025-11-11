81d824d4b2d516d192a9a9530913d8abbf44d524
Mirko Studovic <mirkosec@gmail.com>
2025-11-11 23:07:49 +0100

    Delete Application.java; useless

--- java/leetcode/src/main/java/com/civoduts/Application.java
+++
@@ -1,9 +0,0 @@
-package com.civoduts;
-
-import com.civoduts.solution._207.Solution;
-
-public class Application {
-  public static void main(String[] args) {
-    new Solution().canFinish(2, new int[][]{new int[]{1, 0}});
-  }
-}
