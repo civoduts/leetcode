c8a4e013c71e0631b5e34186db5840d4173ce726
Mirko Studovic <mirkosec@gmail.com>
2025-11-12 13:45:06 +0100

    Solve 210

---
+++ java/leetcode/src/main/java/com/civoduts/solution/_210/Solution.java
@@ -0,0 +1,44 @@
+package com.civoduts.solution._210;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+  public int[] findOrder(int numCourses, int[][] prerequisites) {
+    List<List<Integer>> adj = new ArrayList<>(numCourses);
+    for (int i = 0; i < numCourses; i++) {
+      adj.add(new ArrayList<>());
+    }
+
+    int[] indegrees = new int[numCourses];
+    for (int[] pair : prerequisites) {
+      int course = pair[0];
+      int prereq = pair[1];
+
+      adj.get(prereq).add(course);
+      indegrees[course]++;
+    }
+
+    Queue<Integer> queue = new ArrayDeque<>();
+    for (int i = 0; i < indegrees.length; i++) {
+      if (indegrees[i] == 0)
+        queue.offer(i);
+    }
+
+    int[] order = new int[numCourses];
+    int cnt = 0;
+    while (!queue.isEmpty()) {
+      int course = queue.poll();
+      order[cnt++] = course;
+
+      for (Integer nei : adj.get(course)) {
+        if (--indegrees[nei] == 0)
+          queue.offer(nei);
+      }
+    }
+
+    return cnt == numCourses ? order : new int[0];
+  }
+}
