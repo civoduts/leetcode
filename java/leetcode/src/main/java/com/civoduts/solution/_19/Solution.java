755b5f33410d0cc58b28fe18bd16cdd4c67e0b03
Mirko Studovic <mirkosec@gmail.com>
2025-11-16 19:36:23 +0100

    Solve 19

---
+++ java/leetcode/src/main/java/com/civoduts/solution/_19/Solution.java
@@ -0,0 +1,31 @@
+package com.civoduts.solution._19;
+
+public class Solution {
+  private static ListNode reverse(ListNode head) {
+    ListNode prev = null, curr = head;
+    while (curr != null) {
+      ListNode next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    return prev;
+  }
+
+  // brute-force af
+  public ListNode removeNthFromEnd(ListNode head, int n) {
+    if (head == null)
+      return head;
+    if (n == 1)
+      return reverse(reverse(head).next);
+
+    head = reverse(head);
+    ListNode prev = head;
+    while (--n - 1 > 0) {
+      prev = prev.next;
+    }
+    prev.next = prev.next.next;
+
+    return reverse(head);
+  }
+}
