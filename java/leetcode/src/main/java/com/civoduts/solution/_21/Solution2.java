cc746ff2c8be8324b9ba876ecc3c3530034363c3
Mirko Studovic <mirkosec@gmail.com>
2025-11-16 14:42:36 +0100

    Use dummy head node and tail pointer

---
+++ java/leetcode/src/main/java/com/civoduts/solution/_21/Solution2.java
@@ -0,0 +1,22 @@
+package com.civoduts.solution._21;
+
+public class Solution2 {
+  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    ListNode dummy = new ListNode(0); // temporary head
+    ListNode tail = dummy;
+    while (list1 != null && list2 != null) {
+      if (list1.val < list2.val) {
+        tail.next = list1;
+        list1 = list1.next;
+      }
+      else {
+        tail.next = list2;
+        list2 = list2.next;
+      }
+      tail = tail.next;
+    }
+
+    tail.next = list1 != null ? list1 : list2;
+    return dummy.next;
+  }
+}
