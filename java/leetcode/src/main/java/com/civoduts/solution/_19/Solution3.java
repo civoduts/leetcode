138629e956b1e06f9aedc2c98402f1c36123847c
Mirko Studovic <mirkosec@gmail.com>
2025-11-17 18:29:09 +0100

    Add explanation comment for 19 one pass

--- java/leetcode/src/main/java/com/civoduts/solution/_19/Solution3.java
+++ java/leetcode/src/main/java/com/civoduts/solution/_19/Solution3.java
@@ -6,10 +6,13 @@ public class Solution3 {
     ListNode dummy = new ListNode(0, head);
 
     ListNode slow = dummy, fast = dummy;
-    for (int i = 0; i <= n; i++) {
-      fast = fast.next;
-    }
+    // move fast n+1 nodes ahead
+    for (int i = 0; i <= n; i++) fast = fast.next;
 
+    // move until the end of the list is hit
+    // slow is n+1 nodes behind the end of the list
+    // i.e. slow is one position behind the element
+    // to be removed
     while (fast != null) {
       slow = slow.next;
       fast = fast.next;
