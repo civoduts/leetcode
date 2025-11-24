bacafc6fb97f5594a93d41eb017e61bc4fd7e217
Mirko Studovic <mirkosec@gmail.com>
2025-11-24 22:14:33 +0100

    fix: Calculate max depth of binary tree

--- java/leetcode/src/main/java/com/civoduts/solution/_104/Solution.java
+++ java/leetcode/src/main/java/com/civoduts/solution/_104/Solution.java
@@ -1,20 +1,9 @@
 package com.civoduts.solution._104;
 
 public class Solution {
-  private int maxDepth(TreeNode node, int depth) {
+  public int maxDepth(TreeNode node) {
     if (node == null)
-      return depth;
-
-    int left = maxDepth(node.left, depth);
-    int right = maxDepth(node.right, depth);
-
-    return 1 + Math.max(left, right);
-  }
-
-  public int maxDepth(TreeNode root) {
-    if (root == null)
       return 0;
-
-    return maxDepth(root, 1);
+    return 1 + Math.max(maxDepth(node.left), maxDepth(node.right));
   }
 }
