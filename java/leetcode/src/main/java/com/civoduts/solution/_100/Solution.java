125fd70da709382c4d02394cea4c994b5d4bd5b5
Mirko Studovic <mirkosec@gmail.com>
2025-11-26 16:16:26 +0100

    Compress null checks into one line

--- java/leetcode/src/main/java/com/civoduts/solution/_100/Solution.java
+++ java/leetcode/src/main/java/com/civoduts/solution/_100/Solution.java
@@ -2,11 +2,10 @@ package com.civoduts.solution._100;
 
 public class Solution {
   public boolean isSameTree(TreeNode p, TreeNode q) {
-    if (p == null && q == null) return true;
-    if (p == null) return false;
-    if (q == null) return false;
-    if (p.val != q.val) return false;
-
+    if (p == null || q == null)
+      return p == q;
+    if (p.val != q.val)
+      return false;
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
   }
 }
