ef2d0ea48e330a4352779b7911d4727b8949b055
Mirko Studovic <mirkosec@gmail.com>
2025-12-03 15:57:45 +0100

    Solve 235

---
+++ java/leetcode/src/main/java/_235/Solution.java
@@ -0,0 +1,11 @@
+package _235;
+
+public class Solution {
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (p.val < root.val && q.val < root.val)
+      return lowestCommonAncestor(root.left, p, q);
+    if (root.val < p.val && root.val < q.val)
+      return lowestCommonAncestor(root.right, p, q);
+    return root;
+  }
+}
