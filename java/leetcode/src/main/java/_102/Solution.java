14db90e5f59d4dab7da040605f799539186351e2
Mirko Studovic <mirkosec@gmail.com>
2025-12-03 16:19:46 +0100

    Solve 102

---
+++ java/leetcode/src/main/java/_102/Solution.java
@@ -0,0 +1,31 @@
+package _102;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    if (root == null) return new ArrayList<>();
+
+    Queue<TreeNode> queue = new ArrayDeque<>();
+    queue.offer(root);
+
+    List<List<Integer>> ans = new ArrayList<>();
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      List<Integer> level = new ArrayList<>(size);
+      for (int i = 0; i < size; i++) {
+        TreeNode node = queue.poll();
+        level.add(node.val);
+
+        if (node.left != null) queue.offer(node.left);
+        if (node.right != null) queue.offer(node.right);
+      }
+      ans.add(level);
+    }
+
+    return ans;
+  }
+}
---
+++ java/leetcode/src/main/java/_102/TreeNode.java
@@ -0,0 +1,10 @@
+package _102;
+
+
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int val) {this.val = val;}
+}
