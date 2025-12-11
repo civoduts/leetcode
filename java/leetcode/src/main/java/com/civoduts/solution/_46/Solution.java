c4f4e87bb9f371e55766026cac9da6b4cc1ba82c
Mirko Studovic <mirkosec@gmail.com>
2025-12-11 23:47:36 +0100

    Solve 46

---
+++ java/leetcode/src/main/java/com/civoduts/solution/_46/Solution.java
@@ -0,0 +1,35 @@
+package com.civoduts.solution._46;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+  private void backtrack(
+    int[] nums,
+    boolean[] chosen,
+    List<Integer> perm,
+    List<List<Integer>> ret
+  ) {
+    if (perm.size() == nums.length) {
+      ret.add(new ArrayList<>(perm));
+      return;
+    }
+
+    for (int i = 0; i < nums.length; i++) {
+      if (chosen[i])
+        continue;
+
+      perm.add(nums[i]);
+      chosen[i] = true;
+      backtrack(nums, chosen, perm, ret);
+      perm.removeLast();
+      chosen[i] = false;
+    }
+  }
+
+  public List<List<Integer>> permute(int[] nums) {
+    List<List<Integer>> ans = new ArrayList<>();
+    backtrack(nums, new boolean[nums.length], new ArrayList<>(), ans);
+    return ans;
+  }
+}
