f728f932e774dac50ccc0776c2803012b068fd0e
Mirko Studovic <mirkosec@gmail.com>
2025-12-16 10:16:54 +0100

    Solve 739 left to right

---
+++ java/leetcode/src/main/java/com/civoduts/solution/_739/Solution2.java
@@ -0,0 +1,20 @@
+package com.civoduts.solution._739;
+
+import java.util.Stack;
+
+public class Solution2 {
+  public int[] dailyTemperatures(int[] temperatures) {
+    int[] ans = new int[temperatures.length];
+
+    Stack<Integer> stack = new Stack<>();
+    for (int i = 0; i < temperatures.length; i++) {
+      while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+        int prev = stack.pop();
+        ans[prev] = i - prev;
+      }
+      stack.push(i);
+    }
+
+    return ans;
+  }
+}
