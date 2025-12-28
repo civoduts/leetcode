a34e6329e2cbfc03209c6620f335c73d26244b91
Mirko Studovic <mirkosec@gmail.com>
2025-12-28 14:55:33 +0100

    fix: Make DSU nested class

--- java/leetcode/src/main/java/com/civoduts/solution/_261/Solution4.java
+++ java/leetcode/src/main/java/com/civoduts/solution/_261/Solution4.java
@@ -1,49 +1,50 @@
 package com.civoduts.solution._261;
 
 
-class DSU {
-  private final int[] parent;
-  private final int[] size;
-  private int components;
-
-  public DSU(int n) {
-    parent = new int[n];
-    size = new int[n];
-    components = n;
-    for (int i = 0; i < n; i++) {
-      parent[i] = i;
-      size[i] = 1;
-    }
-  }
-
-  public int getComponents() {
-    return this.components;
-  }
-
-  public int find(int node) {
-    if (this.parent[node] != node)
-      this.parent[node] = find(this.parent[node]);
-    return this.parent[node];
-  }
-
-  public boolean union(int u, int v) {
-    int rootU = find(u), rootV = find(v);
-    // already connected; this forms a cycle
-    if (rootU == rootV) return false;
-    if (this.size[rootU] < this.size[rootV]) {
-      this.parent[rootU] = rootV;
-      this.size[rootV] += this.size[rootU];
-    }
-    else {
-      this.parent[rootV] = rootU;
-      this.size[rootU] += this.size[rootV];
-    }
-    this.components--;
-    return true;
-  }
-}
-
 public class Solution4 {
+  static class DSU {
+    private final int[] parent;
+    private final int[] size;
+    private int components;
+
+    public DSU(int n) {
+      parent = new int[n];
+      size = new int[n];
+      components = n;
+      for (int i = 0; i < n; i++) {
+        parent[i] = i;
+        size[i] = 1;
+      }
+    }
+
+    public int getComponents() {
+      return this.components;
+    }
+
+    public int find(int node) {
+      if (this.parent[node] != node)
+        this.parent[node] = find(this.parent[node]);
+      return this.parent[node];
+    }
+
+    public boolean union(int u, int v) {
+      int rootU = find(u), rootV = find(v);
+      // already connected; this forms a cycle
+      if (rootU == rootV) return false;
+      if (this.size[rootU] < this.size[rootV]) {
+        this.parent[rootU] = rootV;
+        this.size[rootV] += this.size[rootU];
+      }
+      else {
+        this.parent[rootV] = rootU;
+        this.size[rootU] += this.size[rootV];
+      }
+      this.components--;
+      return true;
+    }
+  }
+
+
   public boolean validTree(int n, int[][] edges) {
     DSU dsu = new DSU(n);
     for (int[] edge : edges) {
--- java/leetcode/src/main/java/com/civoduts/solution/_684/Solution.java
+++ java/leetcode/src/main/java/com/civoduts/solution/_684/Solution.java
@@ -1,43 +1,43 @@
 package com.civoduts.solution._684;
 
-class DSU {
-  private final int[] parent, size;
-
-  public DSU(int n) {
-    this.parent = new int[n + 1];
-    this.size = new int[n + 1];
-
-    for (int i = 1; i <= n; i++) {
-      this.parent[i] = i;
-      this.size[i] = 1;
-    }
-  }
-
-  public int find(int node) {
-    if (parent[node] != node)
-      parent[node] = find(parent[node]);
-    return parent[node];
-  }
-
-  public boolean union(int u, int v) {
-    int rootU = find(u), rootV = find(v);
-
-    if (rootU == rootV) return false;
-
-    if (this.size[rootU] < this.size[rootV]) {
-      this.parent[rootU] = rootV;
-      this.size[rootV] += this.size[rootU];
-    }
-    else {
-      this.parent[rootV] = rootU;
-      this.size[rootU] += this.size[rootV];
-    }
-
-    return true;
-  }
-}
-
 public class Solution {
+  static class DSU {
+    private final int[] parent, size;
+
+    public DSU(int n) {
+      this.parent = new int[n + 1];
+      this.size = new int[n + 1];
+
+      for (int i = 1; i <= n; i++) {
+        this.parent[i] = i;
+        this.size[i] = 1;
+      }
+    }
+
+    public int find(int node) {
+      if (parent[node] != node)
+        parent[node] = find(parent[node]);
+      return parent[node];
+    }
+
+    public boolean union(int u, int v) {
+      int rootU = find(u), rootV = find(v);
+
+      if (rootU == rootV) return false;
+
+      if (this.size[rootU] < this.size[rootV]) {
+        this.parent[rootU] = rootV;
+        this.size[rootV] += this.size[rootU];
+      }
+      else {
+        this.parent[rootV] = rootU;
+        this.size[rootU] += this.size[rootV];
+      }
+
+      return true;
+    }
+  }
+
   public int[] findRedundantConnection(int[][] edges) {
     int n = edges.length;
     DSU dsu = new DSU(n);
